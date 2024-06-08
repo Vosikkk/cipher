86df95815eed995355ac15d7be333eaabd61428a
Vosik <vosikvos@gmail.com>
2024-06-08 21:31:17 +0300

    correct name:), started implementing..

--- Package.swift
+++ Package.swift
@@ -4,19 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "cipher",
+    name: "cypher",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/RNCryptor/RNCryptor.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         .executableTarget(
-            name: "cipher",
+            name: "cypher",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "RNCryptor", package: "RNCryptor")
             ]
         ),
-        
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+import ArgumentParser
+import Foundation
+import RNCryptor
+
+
+struct Cypher: ParsableCommand {
+    
+    static let configuration: CommandConfiguration = CommandConfiguration(abstract: "Encrypt and decrypt files")
+}
